24bfe0f6f7d9ede7907347fd58116f4673eac31c
Blaygh <blaike145@gmail.com>
2023-11-05 10:11:34 -0500

    responsive 490px

--- api/server.py
+++ api/server.py
@@ -14,6 +14,28 @@ CORS(app)
 @app.route('/predict', methods=['POST'])
 def predict():
     '''Performs prediction on the text input using the trained model.'''
+# error handling
+    # if request.method != 'POST':
+    #     return jsonify({'error': 'use POST method'}), 405
+    
+    # if request.headers['Content-Type'] != 'application/json':
+    #     return jsonify({'error': 'use JSON format'}), 415
+    
+    # if 'text' not in request.json:                                  
+    #     return jsonify({'error': "no text provided. Make sure key is 'text' "}), 400
+    
+    # if len(request.json) > 1:
+    #     return jsonify({'error': 'only one text input allowed'}), 400
+    
+    # if len(request.json['text']) > 1:
+    #     print(request.json['text'])
+    #     return jsonify({'error': 'only one text input allowed'}), 400
+    
+    # if len(request.json['text']) < 1:
+    #     return jsonify({'error': 'no text provided'}), 400
+    
+    # if request.json['text'] == '':
+    #     return jsonify({'error': 'no text provided'}), 400
 
     # Get the text input from the request
     data = json.loads(request.data)
@@ -23,8 +45,6 @@ def predict():
     print(f'{text} formm') #debugging
 
     # Perform prediction on the text input using the trained model
-    if text == '':
-        return jsonify({'error': 'no text provided'}), 400
     prediction = util.predict([text])
 
     # Send back a response with the prediction
@@ -35,8 +55,23 @@ def predict():
     return response
 
 
+# error handling
+
+# @app.errorhandler(404)
+# def not_found(error):
+#     return jsonify({'error': 'not found'}), 404
+
+# @app.errorhandler(500)
+# def internal_server_error(error):
+#     return jsonify({'error': 'internal server error'}), 500
+
+# @app.errorhandler(400)
+# def bad_request(error):
+#     return jsonify({'error': 'bad request'}), 400
     
 if __name__ == '__main__':
     util.load_data()
     util.vectorizer()
     app.run(debug=True)
+
+
--- ui/script.js
+++ ui/script.js
@@ -9,6 +9,8 @@ const $body = $('#body')
 const $input_card = $('#input-card')
 
 
+
+
 $form.addEventListener('submit', handleSubmit)
 
 function handleSubmit(event) {
@@ -46,10 +48,32 @@ function getPrediction() {
         })
 }
 
-function changeBackground() {
+async function changeBackground() {
     $body.className = 'body_spam'
     $input_card.classList.add('input-main-spam')
 
+    //for the blinkning effect on the input card
+    for (let i = 0; i < 6; i++) {
+        await new Promise(resolve => setTimeout(resolve, 300)); // Wait for 1/3 second
+        $input_card.classList.toggle('input-main-spam-shadow')
+
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1500)); 
+    $body.className = 'body_norm'
+    $input_card.classList.remove('input-main-spam')
+
 }
+
+
+// changing of background
+// setInterval(bg_rotate, 10000);
+
+// function bg_rotate(){
+//     var colors = ['background_2', 'background_3', 'background_4','background_5'];
+//     var random_background = colors[Math.floor(Math.random() * colors.length)];
+
+//     $body.classList.toggle(random_background);
+// }
 
 // Free entry in 2 a wkly comp to win FA Cup final tkts 21st May 2005. Text FA to 87121 to receive entry question(std txt rate)T&C's apply 08452810075over18's
